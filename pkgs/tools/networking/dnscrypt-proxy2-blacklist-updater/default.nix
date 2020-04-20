61e05e277982aa28ec352f502742d00e357dc31c
Your Name <you@example.com>
2020-04-20 10:31:42 -0400

    Initial dnscrypt-proxy2-blacklist-updater working from local repo

--- pkgs/tools/networking/dnscrypt-proxy2-blacklist-updater/default.nix
+++ pkgs/tools/networking/dnscrypt-proxy2-blacklist-updater/default.nix
@@ -1,17 +1,23 @@
-{ lib, python3Packages, fetchFromGitHub }:
+{ stdenv, python3Packages, fetchFromGitHub }:
 
 python3Packages.buildPythonApplication rec {
   pname = "dnscrypt-proxy2";
   version = "2.0.42";
 
-  src = fetchFromGitHub {
-    owner = "jedisct1";
-    repo = "dnscrypt-proxy";
-    rev = version;
-    sha256 = "1v4n0pkwcilxm4mnj4fsd4gf8pficjj40jnmfkiwl7ngznjxwkyw";
-  };
+  src = /home/steve/git/dnscrypt-proxy/utils/generate-domains-blacklists;
 
-  propagatedBuildInputs = with python3Packages; [ tornado_4 python-daemon ];
+  #src = fetchFromGitHub {
+  #  owner = "jedisct1";
+  #  repo = "dnscrypt-proxy";
+  #  rev = version;
+  #  sha256 = "1v4n0pkwcilxm4mnj4fsd4gf8pficjj40jnmfkiwl7ngznjxwkyw";
+  #};
+
+  # Build Dependencies:
+  buildInputs = with python3Packages; [ ];
+
+  # Build AND Runtime Dependencies
+  propagatedBuildInputs = with python3Packages; [ ];
 
   meta = with stdenv.lib; {
     description = "A tool that can be added to crontab for automatically updating dnscrypt-proxy2 blacklists";
