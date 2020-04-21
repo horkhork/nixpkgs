9d837684d6d9bb7857249d5a21ef1aefa1cc0884
Your Name <you@example.com>
2020-04-21 19:22:33 -0400

    Building stock dnscrypt-proxy2 works

--- pkgs/tools/networking/dnscrypt-proxy2-blacklist-updater/default.nix
+++ pkgs/tools/networking/dnscrypt-proxy2-blacklist-updater/default.nix
@@ -5,28 +5,46 @@
 #  - get dnscrypt-proxy2-blacklist-updater into git somewhere and pull it in
 #  - Add crontab entry here with options
 #  - Based on options generate domains-blacklist.conf, domains-whitelist, time-restricted, custom
+#  - actually hook up to dnscrypt-proxy2
 
 python3Packages.buildPythonApplication rec {
   pname = "dnscrypt-proxy2-blacklist-updater";
   version = "2.0.42";
 
-  src = /home/steve/git/dnscrypt-proxy/utils/generate-domains-blacklists;
+  #src = /home/steve/git/dnscrypt-proxy/utils/generate-domains-blacklists;
 
-  #src = fetchFromGitHub {
-  #  owner = "jedisct1";
-  #  repo = "dnscrypt-proxy";
-  #  rev = version;
-  #  sha256 = "1v4n0pkwcilxm4mnj4fsd4gf8pficjj40jnmfkiwl7ngznjxwkyw";
-  #};
+  src = fetchFromGitHub {
+    owner = "jedisct1";
+    repo = "dnscrypt-proxy";
+    rev = version;
+    sha256 = "1v4n0pkwcilxm4mnj4fsd4gf8pficjj40jnmfkiwl7ngznjxwkyw";
+  };
 
-  # # setup.py
-  # from distutils.core import setup
-  # 
-  # setup(
-  #     name='dnscrypt-proxy2-blacklist-updater',
-  #     version='0.0.1',
-  #     scripts=['generate-domains-blacklist.py',],
-  # )
+#  preBuild = ''
+#    echo STEVE PRE BUILD
+#    pwd
+#    ls
+#    cat <<EOF > utils/generate-domains-blacklists/setup.py
+## setup.py
+#from distutils.core import setup
+#
+#setup(
+#    name='dnscrypt-proxy2-blacklist-updater',
+#    version='0.0.1',
+#    scripts=['generate-domains-blacklist.py',],
+#)
+#EOF
+#    ls utils/generate-domains-blacklists/setup.py
+#    cat utils/generate-domains-blacklists/setup.py
+#  '';
+
+  buildPhase = ''
+    echo STEVE BUILD
+  '';
+
+  checkPhase = ''
+    echo STEVE CHECK
+  '';
 
   # Build Dependencies:
   buildInputs = with python3Packages; [ ];
@@ -36,10 +54,10 @@ python3Packages.buildPythonApplication rec {
 
   installPhase = ''
     mkdir -p $out/bin
-    cp $src/generate-domains-blacklist.py $out/bin/generate-domains-blacklist.py
-    cp $src/domains-blacklist.conf $out/domains-blacklist.conf
-    cp $src/domains-time-restricted.txt $out/domains-time-restricted.txt
-    cp $src/domains-whitelist.txt $out/domains-whitelist.txt
+    cp $src/utils/generate-domains-blacklists/generate-domains-blacklist.py $out/bin/generate-domains-blacklist.py
+    cp $src/utils/generate-domains-blacklists/domains-blacklist.conf $out/domains-blacklist.conf
+    cp $src/utils/generate-domains-blacklists/domains-time-restricted.txt $out/domains-time-restricted.txt
+    cp $src/utils/generate-domains-blacklists/domains-whitelist.txt $out/domains-whitelist.txt
   '';
 
   meta = with stdenv.lib; {
